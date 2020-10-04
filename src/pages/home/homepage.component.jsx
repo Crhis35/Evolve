ccc600fe2ee8000c7f110e169eceaec4d16b91c2
crhis <crhis@pop-os.localdomain>
2020-10-04 18:05:17 -0500

    Save changes

--- src/pages/home/homepage.component.jsx
+++ src/pages/home/homepage.component.jsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { HomePageContainer } from './homepage.styles';
 import Directoy from '../../components/directory/directory.component';
 import About from '../../components/about/about.component';
 
-import Aos from 'aos';
 export const HomePage = () => {
-  useEffect(() => {
-    Aos.init({ duration: 2000 });
-  }, []);
   return (
     <HomePageContainer>
       <Directoy />
